27fe4fcb972c7c08ea3b650e9457f19c1e8998c8
algerza <alvaro.ager@gmail.com>
2023-09-05 07:20:44 +0300

    Change variables dynamically in Python

--- app.py
+++ app.py
@@ -59,6 +59,23 @@ def generate_charts():
     max_trend_values_chart2 = []
     min_trend_values_chart2 = []
 
+    logo_url = {
+    "AAPL": "https://www.apple.com/favicon.ico",
+    "GOOGL": "https://www.google.com/favicon.ico",
+    "MSFT": "https://www.microsoft.com/favicon.ico",
+    "AMZN": "https://www.amazon.com/favicon.ico",
+    "META": "https://www.facebook.com/favicon.ico",
+    "BABA": "https://data.alibabagroup.com/ecms-files/886024452/296d05a1-c52a-4f5e-abf2-0d49d4c0d6b3.png",
+    "WMT": "https://www.walmart.com/favicon.ico",
+    "TSLA": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Tesla_T_symbol.svg/1200px-Tesla_T_symbol.svg.png",
+    "INTC": "https://www.intel.com/favicon.ico",
+    "GE": "https://www.ge.com/favicon.ico",
+    }
+
+    selected_company_name = ""
+    selected_days = ""
+    selected_logo_url = ""
+
 
     if request.method == 'POST':
         company_name = request.form['company_name']
@@ -81,9 +98,26 @@ def generate_charts():
         trend_values_chart2 = round(run_model(company_name, 100).yhat.tail(100),2).to_list()
         min_trend_values_chart2 = round(run_model(company_name, 100).yhat_lower.tail(100),2).to_list()
 
-        
+        # Update the selected company name, predicted days, and logo URL
+        selected_company_name = {
+            "AAPL": "Apple Inc.",
+            "GOOGL": "Alphabet Inc.",
+            "MSFT": "Microsoft Inc.",
+            "AMZN": "Amazon Inc.",
+            "META": "Meta Platforms, Inc.",
+            "BABA": "Alibaba Group Holding Ltd",
+            "WMT": "Walmart Inc.",
+            "TSLA": "Tesla Inc.",
+            "INTC": "Intel Corporation",
+            "GE": "General Electric Company",
+            # Add more names as needed
+        }.get(company_name, "")
+        selected_days = str(period_to_predict) + " days"
+        selected_logo_url = logo_url.get(company_name, "")
+
     return render_template('chart.html', x_axis_chart1=x_axis_chart1, close_values_chart1=close_values_chart1, trend_values_chart1=trend_values_chart1, 
-    x_axis_chart2=x_axis_chart2, close_values_chart2=close_values_chart2, trend_values_chart2=trend_values_chart2, max_trend_values_chart2 = max_trend_values_chart2, min_trend_values_chart2 = min_trend_values_chart2)
+    x_axis_chart2=x_axis_chart2, close_values_chart2=close_values_chart2, trend_values_chart2=trend_values_chart2, max_trend_values_chart2 = max_trend_values_chart2, min_trend_values_chart2 = min_trend_values_chart2,
+    logo_url=logo_url, selected_company_name=selected_company_name, selected_days=selected_days, selected_logo_url=selected_logo_url)
 
 
 
